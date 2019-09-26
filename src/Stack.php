435f3c49724d262f7b7987172dcfe208f18a448a
Lviu Codrut Enache <ecodrutz@gmail.com>
2019-09-26 20:27:07 +0300

    Added firsts tests and CI integration.
    Added firsts tests and CI integration.
    
    Added firsts tests and CI integration.
    
    Added firsts tests and CI integration.
    
    Added firsts tests and CI integration.

---
+++ src/Stack.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Stack;
+
+class Stack
+{
+
+}
---
+++ tests/StackTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Stack\Tests;
+
+use Stack\Stack;
+
+class StackTest extends BaseTest
+{
+    public function testTest()
+    {
+        $this->assertTrue(true);
+    }
+}
